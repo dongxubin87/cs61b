e9ee89a8d018d5f5f0e7d17657203390424fa906
dongxubin87 <dongxubin87@gmail.com>
2025-05-14 23:28:44 +0300

    proja1

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,32 +1,37 @@
 public class ArrayDeque<T> {
     private T[] items;
-    private int size;
+    private int size; // the number 0f elements
     private int nextFirst;
     private int nextLast;
-    private int capacity;
-
+    private int capacity;// the length of ArrayDeque
+    // Initialize ArrayDeque
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
-        nextFirst = 0;
-        nextLast = 1;
-        capacity = 8;
+        nextFirst = 0;// it can be any number(0~7)
+        nextLast = 1; //  nextFirst + 1
+        capacity = 8; // default length of array
     }
 
-    public void resize(int x) {
-        T[] temp = (T[]) new Object[x * 2];
+    /** I made some mistakes here,
+     * oldArray = {e,f,g,null,null,a,b,c}
+     * then we get newArray = {a,b,c,e,f,g,null,null,null,...}
+     */
+    private void resize(int x) {
+        T[] temp = (T[]) new Object[x];
         for (int i = 0; i < size; i++, nextFirst++) {
-            temp[0] = items[(nextFirst + 1) % capacity];
+            temp[i] = items[(nextFirst + 1) % capacity];
         }
+        capacity = x;
         items = temp;
-        nextLast = nextFirst;
+        nextLast = size;
         nextFirst = capacity - 1;
 
     }
 
     public void addFirst(T item) {
         if (size == capacity) {
-            resize(capacity);
+            resize(capacity * 2);// when needed, double capacity
         }
         items[nextFirst] = item;
         nextFirst = (nextFirst - 1 + capacity) % capacity;
@@ -34,8 +39,8 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T item) {
-        if (size == 0) {
-            resize(capacity);
+        if (size == capacity) {
+            resize(capacity * 2);
         }
         items[nextLast] = item;
         nextLast = (nextLast + 1) % capacity;
@@ -56,7 +61,7 @@ public class ArrayDeque<T> {
         }
         int curFirst = nextFirst;
         for (int i = 0; i < size; i++, curFirst++) {
-            System.out.println(items[(curFirst + 1) % size] + " ");
+            System.out.print(items[(curFirst + 1) % capacity] + " ");
         }
     }
 
@@ -64,10 +69,15 @@ public class ArrayDeque<T> {
         if (isEmpty()) {
             return null;
         }
+
+        T removedValue = items[(nextFirst + 1) % capacity];
+        items[(nextFirst + 1) % capacity] = null;
+        nextFirst = (nextFirst + 1) % capacity;
+
         size--;
-        T removedValue = items[(nextFirst + 1) % size];
-        items[(nextFirst + 1) % size] = null;
-        nextFirst = (nextFirst + 1) % size;
+        if ((double) size / capacity <= 0.25 && capacity >= 16) {
+            resize(capacity / 2);// here I half the capacity
+        }
         return removedValue;
     }
 
@@ -75,10 +85,22 @@ public class ArrayDeque<T> {
         if (isEmpty()) {
             return null;
         }
+
+        T removedValue = items[(nextLast - 1 + capacity) % capacity];
+        items[(nextLast - 1 + capacity) % capacity] = null;
+        nextLast = (nextLast - 1 + capacity) % capacity;
+
         size--;
-        T removedValue = items[(nextLast - 1 + size) % size];
-        items[(nextLast - 1 + size) % size] = null;
-        nextLast = (nextLast - 1 + size) % size;
+        if ((double) size / capacity <= 0.25 && capacity >= 16) {
+            resize(capacity / 2);
+        }
         return removedValue;
+    }
+
+    public T get(int index) {
+        if (isEmpty() || index < 0 || index >= size) {
+            return null;
+        }
+        return items[(nextFirst + 1 + capacity + index) % capacity];
     }
 }
