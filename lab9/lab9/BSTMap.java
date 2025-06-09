66b7d3b81921b002f0ba547c2fcb78db37532b76
dongxubin87 <dongxubin87@gmail.com>
2025-06-09 20:27:22 +0300

    lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -27,7 +28,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     private Node root;  /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
-
+    private Set<K> hsSet;
+    private ULLMap myULLMap;
     /* Creates an empty BSTMap. */
     public BSTMap() {
         this.clear();
@@ -38,72 +40,191 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public void clear() {
         root = null;
         size = 0;
+        myULLMap.clear();
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            return getHelper(key, p.left);
+        } else if (cmp > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return p.value;
+        }
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        return getHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            size++;
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.value = value;
+        }
+        return p;
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        myULLMap.put(key,value);
+        hsSet.add(key);
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
-    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+    /// ///////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        return hsSet;
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
+        myULLMap.remove(key);
+        hsSet.remove(key);
+        root = remove(key, root);
+        return get(key);
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    private Node remove(K key, Node x) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = remove(key, x.left);
+        } else if (cmp > 0) {
+            x.right = remove(key, x.right);
+        } else {
+            size--;
+            if (x.left == null) {
+                return x.right;
+            }
+            if(x.right == null){
+                return x.left;
+            }
+            Node t = x;
+            x = min(x.right);
+
+            Node k = t.right;
+            while(k != null){
+                k = k.left;
+            }
+            x.right = t.right;
+            x.left = t.left;
+
+        }
+        return x;
+    }
+
+    // get the min key
+
+    public K min() {
+        if (root == null) {
+            return null;
+        }
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x == null) {
+            return null;
+        } else {
+            min(x.left);
+        }
+        return x;
+    }
+
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if(key == null || value == null){
+            throw new IllegalArgumentException("key or value in not legal");
+        }
+        if(get(key) != value){
+            throw new IllegalArgumentException("key or value in not legal");
+        }
+        myULLMap.remove(key,value);
+        hsSet.remove(key);
+        root = remove(key,root);
+
     }
+
+
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+       return  new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K>{
+        Set<K> cur;
+        BSTMapIterator(){
+            cur = hsSet;
+        }
+        @Override
+        public boolean hasNext(){
+             cur = myULLMap.;
+            while(cur)
+        }
+        @Override
+        public K next(){
+            while()
+        }
     }
 }
