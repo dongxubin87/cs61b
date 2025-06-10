1b7a6d5dc417dd28c5692895e629275e325c7d47
dongxubin87 <dongxubin87@gmail.com>
2025-06-11 00:02:32 +0300

    lab9

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -108,6 +108,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         for (K k : keySet()) {
             newBuckets[hash(k, newSize)].put(k, get(k));
         }
+        buckets = newBuckets;
 
     }
 
