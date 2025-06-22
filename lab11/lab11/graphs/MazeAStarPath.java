91907e4045c9bec695f5c6529ec26030dbef8fb8
dongxubin87 <dongxubin87@gmail.com>
2025-06-22 15:31:47 +0300

    hw10

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import java.util.PriorityQueue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
@@ -18,20 +20,52 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
+    /**
+     * Estimate of the distance from v to the target.
+     */
     private int h(int v) {
-        return -1;
+        int vX = maze.toX(v);
+        int vY = maze.toY(v);
+        int tX = maze.toX(v);
+        int tY = maze.toY(v);
+
+        return Math.abs(vX - vY) + Math.abs(tX + tY);
     }
 
-    /** Finds vertex estimated to be closest to target. */
+    /**
+     * Finds vertex estimated to be closest to target.
+     */
     private int findMinimumUnmarked() {
         return -1;
         /* You do not have to use this method. */
     }
 
-    /** Performs an A star search from vertex s. */
+    /**
+     * Performs an A star search from vertex s.
+     */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Integer> pq = new PriorityQueue<>((v1, v2) -> Integer.compare(distTo[v1] + h(v1), distTo[v2] + h(v2)));
+        pq.add(s);
+        marked[s] = true;
+
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
+
+            if (current == t) {
+                targetFound = true;
+                return;
+            }
+
+            for (int neighbor : maze.adj(current)) {
+                if (!marked[neighbor]) {
+                    distTo[neighbor] = distTo[current] + 1;
+                    edgeTo[neighbor] = current;
+                    marked[neighbor] = true;
+                    announce();
+                    pq.add(neighbor);
+                }
+            }
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,6 +1,10 @@
 package lab11.graphs;
 
 
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.Arrays;
+
 /**
  * @author Josh Hug
  */
@@ -11,7 +15,7 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
     private Maze maze;
-    private boolean cycleFound = false;
+    private boolean foundCycle = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -19,36 +23,75 @@ public class MazeCycles extends MazeExplorer {
     }
 
     @Override
+//    public void solve() {
+//        // TODO: Your code here!
+//        detectCycle(0, -1);
+//    }
+//
+//    // Helper methods go here
+//    private void detectCycle(int s, int p) {
+//        if (foundCycle) return;
+//        marked[s] = true;
+//        announce();
+//        for (int w : maze.adj(s)) {
+//            if (foundCycle) return;
+//            if (!marked[w]) {
+//                edgeTo[w] = s;
+//                detectCycle(w, s);
+//            } else if (w != p) {
+//                foundCycle = true;
+//                edgeTo[w] = s;
+//
+//                int x = s;
+//                while (x != w && x != -1) {
+//                    int prev = edgeTo[x];
+//                    edgeTo[prev] = x;
+//                    x = prev;
+//                }
+//                announce();
+//                return;
+//            }
+//        }
+//    }
     public void solve() {
-        // TODO: Your code here!
-        detectCycle(0, -1);
-    }
+        Stack<Integer> stack = new Stack<>();
+        int[] parent = new int[maze.V()];
+        Arrays.fill(parent, -1); // -1 表示无父节点
 
-    // Helper methods go here
-    private void detectCycle(int s, int p) {
-        if (cycleFound) return;
-        marked[s] = true;
+        stack.push(0);
+        marked[0] = true;
         announce();
-        for (int w : maze.adj(s)) {
-            if (cycleFound) return;
-            if (!marked[w]) {
-                edgeTo[w] = s;
-                detectCycle(w, s);
-            } else if (w != p) {
-                cycleFound = true;
-                edgeTo[w] = s;
 
-                int x = s;
-                while (x != w && x != -1) {
-                    int prev = edgeTo[x];
-                    edgeTo[prev] = x;
-                    x = prev;
+        while (!stack.isEmpty() && !foundCycle) {
+            int v = stack.pop();
+
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    parent[w] = v;
+                    edgeTo[w] = v;
+                    announce();
+                    stack.push(w);
+                } else if (w != parent[v]) {
+                    // 发现环，w 已访问，且不是 v 的父节点
+                    foundCycle = true;
+
+                    // 构造环路径（从 v 回到 w）
+                    edgeTo[w] = v;
+                    announce();
+
+                    int x = v;
+                    while (x != w && x != -1) {
+                        int p = parent[x];
+                        edgeTo[p] = x;
+                        announce();
+                        x = p;
+                    }
+
+                    return; // 找到环后立即退出
                 }
-                announce();
-                return;
             }
         }
     }
-
 }
 
